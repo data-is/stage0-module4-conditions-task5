407b18687fff2ba11ec14b09509d4ad5d16ec37a
Luka <code.coding2001@gmail.com>
2023-10-15 22:02:50 +0400

    1

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String leap = ((year%1000%100%10) % 4) != 0 ? "not leap" : "leap";
+        System.out.println(leap);
     }
 }
